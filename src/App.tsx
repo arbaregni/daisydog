22332cf02a676618388dfd00ef62d97c6f9a0374
Valorie Sours <jamesnsours@gmail.com>
2025-03-03 21:20:22 -0500

    Prod commands, docker infra

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <h1>Have you seen this dog???</h1>
       <center>
           <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
+            <button type="button" onClick={() => setCount((count) => count + 1)}>
               count is {count}
             </button>
           </div>
