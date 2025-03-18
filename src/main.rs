bae6c424895610bbdc80dab4f958d02c11594d85
Valorie Sours <jamesnsours@gmail.com>
2025-03-17 22:42:33 -0400

    Service static files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+use axum::Router;
+use tower_http::services::ServeDir;
+use tower_http::services::ServeFile;
+
+use tracing_subscriber;
+use tracing::Level;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
-    let app = Router::new().route("/", get(handler));
+    tracing_subscriber::fmt()
+        .with_max_level(Level::INFO)
+        .init();
 
-    // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    // build our application with a route
+    let app = Router::new()
+        .nest_service("/static", ServeDir::new("static"))
+        .fallback_service(ServeFile::new("static/index.html"));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let listener = tokio::net::TcpListener::bind(addr)
         .await
         .unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
-}
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+    log::info!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }
